746443f853ea438dc66cd62b937cc05aac8ba8ef
ninhydrin <yokoshima@yokoshimafu-no-MacBook-Air.local>
2017-06-06 22:44:02 +0900

    create User class

--- twitter.py
+++ twitter.py
@@ -25,10 +25,6 @@ class Twitter:
     users = "https://api.twitter.com/1.1/users/"
     stop_list = ("RT", "http")
 
-    def __init__(self, target_id, data_dir="TimeLine"):
-        self.target_id = target_id
-        self.data_path = "{}/{}.json".format(data_dir, target_id)
-
     @classmethod
     def use_api(cls, num=1):
         cls.use += num
@@ -49,67 +45,6 @@ class Twitter:
         req = json.loads(req.text)
         return req
 
-    def get_user_info(self, entity=False):
-        return self.user_info(self.target_id, entity)
-
-    def get_follow_exchanger(self):
-        follow = self.target_follow_list()
-        follower = self.target_follower_list()
-        if not follow or not follower:
-            return []
-        return list(set(follow["ids"]).intersection(set(follower["ids"])))
-
-    def target_follow_list(self, to_string=True, count=5000):
-        return self.get_follow_list(self.target_id, to_string, count)
-
-    def target_follower_list(self, to_string=True, count=5000):
-        return self.get_followe_list(self.target_id, to_string, count)
-
-    def get_twit(self, exclude_rep=0, include_rt=0, count=200, max_id=None, since_id=None):
-        url = self.statuses + "user_id={}&count={}&exclude_replies={}&include_rts={}".format(self.target_id, count, exclude_rep, include_rt)
-        if max_id:
-            url += "&max_id={}".format(max_id)  # max_id以下を取得
-        if since_id:
-            url += "&since_id={}".format(since_id)  # since_idより上を取得
-        return self.__get_method(url)
-
-    def get_twit_list(self, exclude_rep=0, include_rt=0, count=200, max_id=None, since_id=None):
-        req = self.get_twit(exclude_rep, include_rt, count=200, max_id=None, since_id=None):
-        if req is None:
-            return []
-        return [{x: twit[x] for x in twit
-                 if x == "text"or x == "id_str" or x == "created_at"}
-                for twit in req]
-
-    def save_timeline(self, save=1, rep=0):
-        max_id = None
-        twit_count = 0
-        api_use_count = 0
-        old_timeline = []
-        timeline = []
-        since_id = 0
-        if os.path.exists(self.data_path):
-            old_timeline = json.load(open(self.data_path))
-            since_id = old_timeline[0]["id_str"]
-        else:
-            print("user id ", self.target_id, "is first")
-
-        for i in range(40):
-            api_use_count += 1
-            new_timeline = self.get_twit_list(contain_rep=rep, max_id=max_id, since_id=since_id)
-            if api_use_count > 1:
-                new_timeline = new_timeline[1:]
-            if len(new_timeline) <= 0:
-                break
-            twit_count += len(new_timeline)
-            max_id = new_timeline[-1]["id_str"]
-            timeline += new_timeline
-        timeline += old_timeline
-        if twit_count and save:
-            json.dump(timeline, open(self.data_path, "w"), sort_keys=True, indent=4)
-        print("added:{} tweet  api: {} used".format(twit_count, api_use_count))
-        return api_use_count
-
     @classmethod
     def word_search(cls, text):
         url = "https://api.twitter.com/1.1/search/tweets.json?q={}".format(text)
@@ -127,6 +62,15 @@ class Twitter:
         url = cls.users+"show.json?user_id={}&include_entities={}".format(ids, entity)
         req = cls.__get_method(url)
         return req
+
+    @classmethod
+    def get_twit(cls, ids, exclude_rep=0, include_rt=0, count=200, max_id=None, since_id=None):
+        url = self.statuses + "user_id={}&count={}&exclude_replies={}&include_rts={}".format(ids, count, exclude_rep, include_rt)
+        if max_id:
+            url += "&max_id={}".format(max_id)  # max_id以下を取得
+        if since_id:
+            url += "&since_id={}".format(since_id)  # since_idより上を取得
+        return cls.__get_method(url)
 
     @classmethod
     def get_follow_list(cls, target_id, to_string=True, count=5000):
@@ -155,3 +99,28 @@ class Twitter:
         req = cls.twitter_oauth.post(url, params=params)
         return req if req.status_code == 200 else False
 
+
+class User:
+    def __init__(self, target_id, data_dir="TimeLine"):
+        self.target_id = target_id
+        self.data_path = "{}/{}.json".format(data_dir, target_id)
+        self.twitter = Twitter
+
+    def user_info(self, entity=False):
+        return self.user_info(self.target_id, entity)
+
+    def follow_exchanger(self):
+        follow = self.target_follow_list()
+        follower = self.target_follower_list()
+        if not follow or not follower:
+            return []
+        return list(set(follow["ids"]).intersection(set(follower["ids"])))
+
+    def follow_list(self, to_string=True, count=5000):
+        return self.twitter.get_follow_list(self.target_id, to_string, count)
+
+    def follower_list(self, to_string=True, count=5000):
+        return self.twitter.get_followe_list(self.target_id, to_string, count)
+
+    def get_twit(self, ids, exclude_rep=0, include_rt=0, count=200, max_id=None, since_id=None):
+        return self.twitter.get_twit(ids, exclude_rep, include_rt, count, max_id, since_id)
